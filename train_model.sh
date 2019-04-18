c69fdcab24bcf41afa902d626c0b5a42d4a38578
Kevin Lu <kevinyihchyunlu@gmail.com>
2019-04-18 11:01:58 +1000

    added dvc repro

--- train_model.sh
+++ train_model.sh
@@ -2,6 +2,7 @@ git pull origin master
 branch_name=training_run/$(date +%x/%H/%M/%S)
 git checkout -b $branch_name
 aws s3 sync s3://dvc-root-data-bucket ./data
+dvc repro
 git add .
 git push origin $branch_name
 git checkout master
