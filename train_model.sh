5562caa46f953dffcdbead6c09b5c867c00c5858
Kevin <klu@straxcorp.com>
2019-04-18 14:43:20 +1000

    finished training run

--- train_model.sh
+++ train_model.sh
@@ -4,5 +4,6 @@ git checkout -b $branch_name
 aws s3 sync s3://dvc-root-data-bucket ./data
 dvc repro
 git add .
+git commit -m "finished training run"
 git push origin $branch_name
 git checkout master
