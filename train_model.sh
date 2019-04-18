108e933954fe87af6b5f6a7a430b76b7a95c1f65
Kevin Lu <kevinyihchyunlu@gmail.com>
2019-04-18 10:54:33 +1000

    added train_model.sh

---
+++ train_model.sh
@@ -0,0 +1,7 @@
+git pull origin master
+branch_name=$(date +%x/%H/%M/%S)
+git checkout -b training_run/$(branch_name)
+aws s3 cp s3://dvc-root-data-bucket ./data --recursive
+git add .
+git push origin $(branch_name)
+git checkout master
