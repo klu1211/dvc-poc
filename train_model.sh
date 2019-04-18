072d30f690f682f6698e6b9feb22ee6e3db3a60e
Kevin Lu <kevinyihchyunlu@gmail.com>
2019-04-18 11:00:36 +1000

    removed recursive flag

--- train_model.sh
+++ train_model.sh
@@ -1,7 +1,7 @@
 git pull origin master
 branch_name=$(date +%x/%H/%M/%S)
 git checkout -b training_run/$branch_name
-aws s3 sync s3://dvc-root-data-bucket ./data --recursive
+aws s3 sync s3://dvc-root-data-bucket ./data
 git add .
 git push origin $branch_name
 git checkout master
